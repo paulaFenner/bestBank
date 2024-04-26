d6ffb4183895ef7c12819a3bcb0c894ac932b322
Paula Fenner <fenner.paula@gmail.com>
2024-04-26 11:45:26 +0200

    added side menu on mobile

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ import { accounts } from './data.js';
 
 const accountsContainerEl = document.getElementById('accounts-container');
 const spendingsContainerEl = document.getElementById('spendings-container');
+const sideMenuEl = document.getElementById('side-menu');
+const hamburgerMenuEl = document.getElementById('hamburger-menu');
 
 accountsContainerEl.addEventListener('click', handleAccountClick);
+hamburgerMenuEl.addEventListener('click', toggleMenu);
+
+function toggleMenu(e) {
+  e.preventDefault(); // Prevent the default behavior of the <a> tag
+  sideMenuEl.classList.toggle('show-menu');
+}
 
 function renderAccounts() {
   accountsContainerEl.innerHTML = accounts.map(getAccountHtml).join('');
 }
 
 function handleAccountClick(e) {
+  // Find the closest ancestor with the 'account' class and get its id
   const selectedAccountId = e.target.closest('.account')?.id;
   const selectedAccount = findAccountById(selectedAccountId);
   showSpendings(selectedAccount);
@@ -28,21 +37,25 @@ function getAccountHtml(account) {
   `;
 }
 
+// Show spendings for the selected account
 function showSpendings(selectedAccount) {
   if (!selectedAccount) {
-    clearSelection();
+    noSelection();
     return;
   }
 
+  // If the selected account has no spendings, display a message
   const { spendings } = selectedAccount;
   if (!spendings.length) {
     spendingsContainerEl.innerHTML = `<p class="no-spendings">No spendings for this account 💰💰💰</p>`;
     return;
   }
 
+  // Generate HTML for each spending and join them together
   spendingsContainerEl.innerHTML = spendings.map(getSpendingHtml).join('');
 }
 
+// Generate HTML for a spending
 function getSpendingHtml(spending) {
   return `
     <div class="spending">
@@ -52,7 +65,7 @@ function getSpendingHtml(spending) {
   `;
 }
 
-function clearSelection() {
+function noSelection() {
   spendingsContainerEl.innerHTML = `<p class="no-spendings">No account selected</p>`;
 }
 
@@ -64,5 +77,6 @@ function addCopyright() {
   footer.innerHTML = `<p>&copy; ${currentYear} BestBank. All rights reserved.</p>`;
 }
 
+// Render the accounts and add copyright information
 renderAccounts();
 addCopyright();
